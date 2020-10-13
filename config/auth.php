1d2ec63401ca8d3398419ae59427a080d7039a13
Dmitry Zorin <budteschastlivi1@gmail.com>
2020-10-13 19:48:26 +0300

    remove files

--- config/auth.php
+++ config/auth.php
@@ -71,10 +71,10 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'client' => [
+            'driver' => 'eloquent',
+            'table' => App\Models\Client::class,
+        ],
     ],
 
     /*
